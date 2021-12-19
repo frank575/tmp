6ae009e2e3eb4472627babf6fa82ae2f5d8ccb97
wuchunwei <wcw850710@gmail.com>
2021-12-19 21:46:09 +0800

    :label: 補上 react-native-lib 類型

---
+++ js/575/packages/react-native-lib/type.d.ts
@@ -0,0 +1,13 @@
+declare module '@575/react-common-lib' {
+	import { Dispatch, SetStateAction } from 'react'
+
+	export function useAsyncStorageStore<T extends Object, K extends keyof T>(
+		appName: string,
+		initialStore: T,
+	): [T, (key: K, value: T[K]) => void]
+
+	export function useAsyncStorageState<T>(
+		key: string,
+		initialValue: T,
+	): [T, Dispatch<SetStateAction<T>>, () => Promise<void>]
+}
